2bab97d8cd8b5f1205ea5da0eb3fdcb77ba3a55d
Clementine PIRLOT <cpirlot@e2r4p2.42.fr>
2018-04-03 13:32:47 +0200

    WIP : problem with elevator's direction

--- elevator.rb
+++ elevator.rb
@@ -3,25 +3,39 @@ NB_FLOORS = 10
 
 class ElevatorCall
     attr_reader :nb, :source_floor, :going_up
-    attr_accessor :dest_floor
     def initialize(nb, source_floor, going_up)
         @nb = nb
         @source_floor = source_floor
         @going_up = going_up
-        @dest_floor = nil
     end
     def to_s
         "Call from person #{nb} at floor #{@source_floor} going #{@going_up ? "up" : "down"}"
     end
 end
 
+class ElevatorRider
+    attr_reader :nb, :dest_floor
+    def initialize(nb)
+        @nb = nb
+        @dest_floor = nil
+    end
+    def gets_in(going_up, current_floor)
+        if going_up
+            @dest_floor = Random.rand(current_floor+1..NB_FLOORS)
+        else
+            @dest_floor = Random.rand(0..current_floor-1)
+        end
+        puts "Person #{@nb} gets in, is going to floor #{@dest_floor}"
+    end
+end
+
 class Elevator
-    attr_reader :current_floor, :going_up, :calls_up, :calls_down
+    attr_reader :current_floor, :going_up, :calls, :riders, :dest_floor
     def initialize
         @current_floor = 0
-        @calls_up = {}
-        @calls_down = {}
-        @current_call = nil
+        @dest_floor = 0
+        @calls = {}
+        @riders = []
         @going_up = true
         @mutex = Mutex.new
     end
@@ -29,59 +43,51 @@ class Elevator
         while 42
             sleep(1)
             @mutex.synchronize {
-                if @calls_up.empty? && @calls_down.empty?
+                if @riders.empty? && @calls.empty?
                     puts "Elevator is at floor #{@current_floor}"
                 end
-                if @current_call
-                    while @current_floor != @current_call.source_floor
-                        @current_floor += 1
-                        puts "Elevator going to floor #{@current_floor}"
-                        sleep(1)
+            }
+            @mutex.synchronize {
+                while @current_floor != @dest_floor
+                    # @going_up = @dest_floor > @current_floor ? true : false
+                    @going_up ? @current_floor +=1 : @current_floor -= 1
+                    puts "Elevator going to floor #{@current_floor}"
+                    if @calls[@current_floor]
+                        @calls[@current_floor].each { |call| 
+                            if call.going_up == @going_up && call.source_floor == @current_floor
+                                rider = ElevatorRider.new(call.nb)
+                                rider.gets_in(call.going_up, @current_floor)
+                                if !riders.include?(rider)
+                                    @riders << rider
+                                    if @calls[call.source_floor].length > 1
+                                        @calls[call.source_floor].delete_at(call.nb - 1)
+                                    else
+                                        @calls.delete(call.source_floor)
+                                    end
+                                end
+                                if @dest_floor == @current_floor
+                                    @dest_floor = rider.dest_floor
+                                end
+                            end
+                        }
                     end
-                    if @current_call.going_up
-                        @current_call.dest_floor = Random.rand(@current_floor+1..NB_FLOORS)
-                    else
-                        @current_call.dest_floor = Random.rand(0..@current_floor-1)
-                    end
-                    puts "Person #{@current_call.nb} gets in, is going to floor #{@current_call.dest_floor}"
-                    while @current_floor != @current_call.dest_floor
-                        @current_call.going_up ? @current_floor += 1 : @current_floor -=1
-                        puts "Elevator going to floor #{@current_floor}"
-                    end
-                    puts @calls_up.inspect
-                    puts @calls_down.inspect
-                    if @current_call.going_up
-                        if @calls_up[@current_call.source_floor].length > 1
-                            @calls_up[@current_call.source_floor].delete_at(@current_call.nb - 1)
-                        else
-                            @calls_up.delete(@current_call.source_floor)
+                    @riders.each { |rider| 
+                        if rider.dest_floor == @current_floor
+                            puts "Person #{rider.nb} gets out at floor #{@current_floor}"
+                            riders.delete(rider)
                         end
-                    else
-                        if @calls_down[@current_call.source_floor].length > 1
-                            @calls_down[@current_call.source_floor].delete_at(@current_call.nb - 1)
-                        else
-                            @calls_down.delete(@current_call.source_floor)
-                        end
-                    end
-                    puts @calls_up.inspect
-                    puts @calls_down.inspect
-                    @current_call = nil
+                    }
                 end
             }
-        end
     end
+end
     def insert_call(call)
-        if @current_call == nil
-            @current_call = call
-        end
         @mutex.synchronize {
-            if call.going_up
-                @calls_up[call.source_floor] ||= []
-                @calls_up[call.source_floor] << call
-            else
-                @calls_down[call.source_floor] ||= []
-                @calls_down[call.source_floor] << call
+            if @riders.empty?
+                @dest_floor = call.source_floor
             end
+            @calls[call.source_floor] ||= []
+            @calls[call.source_floor] << call
         }
     end
 end
